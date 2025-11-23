7a9bf7e9de900d841c7a07d9c83bb256a43e6df0
Enes Ay <enesay233@gmail.com>
2025-11-23 21:52:23 +0300

    Update repository module

--- app/src/main/java/com/enesay/android_task/di/RepositoryModule.kt
+++ app/src/main/java/com/enesay/android_task/di/RepositoryModule.kt
@@ -2,19 +2,19 @@ package com.enesay.android_task.di
 
 import com.enesay.android_task.data.repository.TaskRepositoryImpl
 import com.enesay.android_task.domain.repository.TaskRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideTaskRepository(
+    abstract fun bindTaskRepository(
         impl: TaskRepositoryImpl
-    ): TaskRepository = impl
+    ): TaskRepository
 }
