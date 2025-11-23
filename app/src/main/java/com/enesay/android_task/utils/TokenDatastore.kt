e39fd46121ab6821c61fb44c653b755fa6b9b89a
Enes Ay <enesay233@gmail.com>
2025-11-23 16:46:11 +0300

    Added datastore implementation for storing token

---
+++ app/src/main/java/com/enesay/android_task/utils/TokenDatastore.kt
@@ -0,0 +1,52 @@
+package com.enesay.android_task.utils
+
+import android.content.Context
+import androidx.datastore.core.IOException
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private val Context.dataStore by preferencesDataStore("auth_prefs")
+
+@Singleton
+class TokenDatastore @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    companion object {
+        private val TOKEN_KEY = stringPreferencesKey("access_token")
+    }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
+
+    fun getToken(): Flow<String?> {
+        return context.dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                preferences[TOKEN_KEY]
+            }
+    }
+
+    suspend fun clearToken() {
+        context.dataStore.edit { preferences ->
+            preferences.remove(TOKEN_KEY)
+        }
+    }
+}
