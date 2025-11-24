07cba888a72eee1d55afd1fadaf8cfdd36769ed4
Enes Ay <enesay233@gmail.com>
2025-11-24 23:45:18 +0300

    Add dispatcher injection and refactor for testable environment

--- app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListViewModel.kt
+++ app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListViewModel.kt
@@ -2,17 +2,21 @@ package com.enesay.android_task.ui.screens.TaskList
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.enesay.android_task.di.IoDispatcher
 import com.enesay.android_task.domain.model.TaskModel
 import com.enesay.android_task.domain.usecase.GetTasksUseCase
 import com.enesay.android_task.domain.usecase.RefreshTasksUseCase
 import com.enesay.android_task.domain.usecase.SearchTasksUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,25 +25,26 @@ import javax.inject.Inject
 class TaskListViewModel @Inject constructor(
     private val getTasksUseCase: GetTasksUseCase,
     private val searchTasksUseCase: SearchTasksUseCase,
-    private val refreshTasksUseCase: RefreshTasksUseCase
+    private val refreshTasksUseCase: RefreshTasksUseCase,
+    @IoDispatcher ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
-
-    init {
-        getTasksUseCase()
-        viewModelScope.launch {
-            refreshTasksUseCase()
-        }
-    }
 
     private val _query = MutableStateFlow("")
     //private val _state = MutableStateFlow<TaskListState>(TaskListState.Empty)
     private val _isRefreshing = MutableStateFlow(false)
     val isRefreshing: StateFlow<Boolean> = _isRefreshing.asStateFlow()
 
-    val tasks: StateFlow<List<TaskModel>> = _query.debounce(300) // typing debounce
+    val tasks: StateFlow<List<TaskModel>> = _query
+        .debounce(300) // typing debounce
         .flatMapLatest { q ->
             if (q.isBlank()) getTasksUseCase() else searchTasksUseCase(q)
-        }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+        }
+        .flowOn(ioDispatcher)
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
 
     fun setQuery(q: String) {
         _query.value = q
