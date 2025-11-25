cb6fe87665116e3c7f238c3ba5751fbf39a0aa9b
Enes Ay <enesay233@gmail.com>
2025-11-25 23:43:53 +0300

    Refactor Task Repository

--- app/src/main/java/com/enesay/android_task/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/enesay/android_task/data/repository/TaskRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.enesay.android_task.data.repository
 
-import com.enesay.android_task.data.LoginRequest
 import com.enesay.android_task.data.local.TaskDAO
 import com.enesay.android_task.data.mapper.toDomain
 import com.enesay.android_task.data.mapper.toEntity
 import com.enesay.android_task.data.remote.ApiService
 import com.enesay.android_task.domain.model.TaskModel
+import com.enesay.android_task.domain.repository.AuthRepository
 import com.enesay.android_task.domain.repository.TaskRepository
-import com.enesay.android_task.utils.PASSWORD
-import com.enesay.android_task.utils.TokenDatastore
-import com.enesay.android_task.utils.USERNAME
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import retrofit2.HttpException
@@ -18,7 +15,7 @@ import javax.inject.Inject
 class TaskRepositoryImpl @Inject constructor(
     private val taskDao: TaskDAO,
     private val apiService: ApiService,
-    private val dataStoreManager: TokenDatastore
+    private val authRepository: AuthRepository
 ) : TaskRepository {
 
     override fun getTasks(): Flow<List<TaskModel>> {
@@ -34,39 +31,23 @@ class TaskRepositoryImpl @Inject constructor(
     }
 
     override suspend fun updateTasks() {
+
+        val isAuthReady = authRepository.ensureValidToken()
+
+        if (!isAuthReady) {
+            throw Exception("Invalid Session, login again!")
+        }
+
         try {
-            fetchAndSaveFromRemote()
+            val remoteTasks = apiService.getTasks()
+            val entities = remoteTasks.map { it.toEntity() }
+            taskDao.updateTasks(entities)
+
         } catch (e: HttpException) {
             if (e.code() == 401) {
-                performAutoLoginAndRetry()
-            } else {
-                e.printStackTrace()
+                // performAutoLoginAndRetry()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
+            throw e
         }
-    }
-
-    private suspend fun performAutoLoginAndRetry() {
-        try {
-            val loginResponse = apiService.login(
-                LoginRequest(USERNAME, PASSWORD)
-            )
-            if (loginResponse.oauth != null) {
-                val newToken = loginResponse.oauth.access_token
-                if (newToken != null) {
-                    dataStoreManager.saveToken(newToken)
-                    fetchAndSaveFromRemote()
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private suspend fun fetchAndSaveFromRemote() {
-        val tasks = apiService.getTasks()
-        val entities = tasks.map { it.toEntity() }
-        taskDao.updateTasks(entities)
     }
 }
