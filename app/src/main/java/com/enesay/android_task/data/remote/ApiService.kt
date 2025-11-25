a401f55632db8e4d32a8c5d25f2f45cfa5776b33
Enes Ay <enesay233@gmail.com>
2025-11-25 23:34:26 +0300

    Add login service headers

--- app/src/main/java/com/enesay/android_task/data/remote/ApiService.kt
+++ app/src/main/java/com/enesay/android_task/data/remote/ApiService.kt
@@ -1,9 +1,10 @@
 package com.enesay.android_task.data.remote
 
-import com.enesay.android_task.data.LoginRequest
 import com.enesay.android_task.domain.model.LoginResponse
+import com.enesay.android_task.utils.LOGIN_TOKEN
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
@@ -11,6 +12,10 @@ interface ApiService {
     @GET("v1/tasks/select")
     suspend fun getTasks(): List<TaskDTO>
 
+    @Headers(
+        "Authorization: Basic $LOGIN_TOKEN",
+        "Content-Type: application/json"
+    )
     @POST("/index.php/login")
     suspend fun login(@Body creds: LoginRequest): LoginResponse
 }
