8dc4a756aa2b464a33921f11ced5934201f7913e
Enes Ay <enesay233@gmail.com>
2025-11-25 13:16:29 +0300

    Update task list state handling implemented

--- app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListScreen.kt
+++ app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListScreen.kt
@@ -1,23 +1,35 @@
 package com.enesay.android_task.ui.screens.TaskList
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.enesay.android_task.domain.model.TaskModel
@@ -28,56 +40,117 @@ import com.enesay.android_task.utils.startQrCodeScanner
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskListScreen(viewModel: TaskListViewModel = hiltViewModel(), innerPadding: PaddingValues) {
-    val tasks by viewModel.tasks.collectAsState()
-    var searchQuery by rememberSaveable { mutableStateOf("") }
-    val isRefreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
-    var isSearchActive by remember { mutableStateOf(false) }
+fun TaskListScreen(
+    viewModel: TaskListViewModel = hiltViewModel(),
+    innerPadding: PaddingValues
+) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
 
-    Scaffold(
-        topBar = {
-            // Deciding which topbar will be used
-            if (isSearchActive) {
-                SearchTopBar(text = searchQuery, onTextChange = {
-                    searchQuery = it
-                    viewModel.setQuery(it)
-                }, onCloseClicked = {
-                    isSearchActive = false
-                    viewModel.setQuery("")
-                }, onQrClicked = {
-                    startQrCodeScanner(
-                        context = context,
-                        onResult = {
-                            searchQuery = it
-                            viewModel.setQuery(it)
-                        })
-                })
-            } else {
-                DefaultTopBar(onSearchClicked = { isSearchActive = true }, onQrClicked = {
-                    startQrCodeScanner(
-                        context = context,
-                        onResult = {
-                            isSearchActive = true
-                            searchQuery = it
-                            viewModel.setQuery(it)
-                        })
-                })
+    LaunchedEffect(state.contentState) {
+        if (state.contentState is TaskContentState.Error) {
+            Toast.makeText(
+                context,
+                (state.contentState as TaskContentState.Error).message,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+    // One-Shot event
+    LaunchedEffect(Unit) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                is TaskListEffect.ShowToast -> {
+                    Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
+                }
             }
-        }) { paddingValues ->
+        }
+    }
 
-        Column(
+    TaskListContent(
+        state = state,
+        onEvent = viewModel::onEvent,
+        paddingValues = innerPadding,
+        context = context
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskListContent(
+    state: TaskListUiState,
+    onEvent: (TaskListUiEvent) -> Unit,
+    paddingValues: PaddingValues,
+    context: Context
+) {
+
+    val launchQrScanner = {
+        startQrCodeScanner(
+            context = context,
+            onResult = { code ->
+                onEvent(TaskListUiEvent.OnSearchActiveChange(true))
+                onEvent(TaskListUiEvent.OnSearchQueryChange(code))
+            },
+            onError = { errorMessage ->
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+
+    Scaffold(
+        // Deciding which topbar will be used
+        topBar = {
+            if (state.isSearchActive) {
+                SearchTopBar(
+                    text = state.searchQuery,
+                    onTextChange = { onEvent(TaskListUiEvent.OnSearchQueryChange(it)) },
+                    onCloseClicked = { onEvent(TaskListUiEvent.OnSearchActiveChange(false)) },
+                    onQrClicked = { launchQrScanner() }
+                )
+            } else {
+                DefaultTopBar(
+                    onSearchClicked = { onEvent(TaskListUiEvent.OnSearchActiveChange(true)) },
+                    onQrClicked = { launchQrScanner() }
+                )
+            }
+        }
+    ) { scaffoldPadding ->
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .padding(scaffoldPadding)
         ) {
+            when (val content = state.contentState) {
+                // Data Loading
+                is TaskContentState.Loading -> {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
 
-            PullToRefreshBox(
-                isRefreshing = isRefreshing,
-                onRefresh = { viewModel.refresh() },
-                modifier = Modifier.weight(1f)
-            ) {
-                TaskList(tasks = tasks)
+                // Empty View
+                is TaskContentState.Empty -> {
+                    EmptyView(modifier = Modifier.align(Alignment.Center))
+                }
+
+                // Error Retry Button
+                is TaskContentState.Error -> {
+                    ErrorView(
+                        message = content.message,
+                        onRetry = { onEvent(TaskListUiEvent.OnRefresh) },
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                // Success
+                is TaskContentState.Success -> {
+                    PullToRefreshBox(
+                        isRefreshing = state.isRefreshing, // Loading'i zaten yukarıda yönetiyoruz
+                        onRefresh = { onEvent(TaskListUiEvent.OnRefresh) },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        TaskList(tasks = content.tasks)
+                    }
+                }
             }
         }
     }
@@ -85,13 +158,49 @@ fun TaskListScreen(viewModel: TaskListViewModel = hiltViewModel(), innerPadding:
 
 @Composable
 fun TaskList(
-    tasks: List<TaskModel>, modifier: Modifier = Modifier
+    tasks: List<TaskModel>,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
     ) {
-        items(tasks) { task ->
+        items(
+            items = tasks,
+            key = { task -> task.taskId }
+        ) { task ->
             TaskRowItem(task = task)
+        }
+    }
+}
+
+@Composable
+fun EmptyView(modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(
+            Icons.Default.Info,
+            contentDescription = null,
+            modifier = Modifier.size(64.dp),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("Tasks not found.")
+    }
+}
+
+@Composable
+fun ErrorView(message: String, onRetry: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(
+            Icons.Default.Warning,
+            contentDescription = null,
+            modifier = Modifier.size(64.dp),
+            tint = MaterialTheme.colorScheme.error
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = message, color = MaterialTheme.colorScheme.error)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onRetry) {
+            Text("Retry")
         }
     }
 }
--- app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListViewModel.kt
+++ app/src/main/java/com/enesay/android_task/ui/screens/TaskList/TaskListViewModel.kt
@@ -3,20 +3,26 @@ package com.enesay.android_task.ui.screens.TaskList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.enesay.android_task.di.IoDispatcher
-import com.enesay.android_task.domain.model.TaskModel
 import com.enesay.android_task.domain.usecase.GetTasksUseCase
 import com.enesay.android_task.domain.usecase.RefreshTasksUseCase
 import com.enesay.android_task.domain.usecase.SearchTasksUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,39 +32,100 @@ class TaskListViewModel @Inject constructor(
     private val getTasksUseCase: GetTasksUseCase,
     private val searchTasksUseCase: SearchTasksUseCase,
     private val refreshTasksUseCase: RefreshTasksUseCase,
-    @IoDispatcher ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
 
-    private val _query = MutableStateFlow("")
-    //private val _state = MutableStateFlow<TaskListState>(TaskListState.Empty)
+    private val _searchQuery = MutableStateFlow("")
+    private val _isSearchActive = MutableStateFlow(false)
+    private val _effect = Channel<TaskListEffect>()
+    val effect = _effect.receiveAsFlow()
+    private val _isFirstLoad = MutableStateFlow(true)
     private val _isRefreshing = MutableStateFlow(false)
-    val isRefreshing: StateFlow<Boolean> = _isRefreshing.asStateFlow()
 
-    val tasks: StateFlow<List<TaskModel>> = _query
-        .debounce(300) // typing debounce
-        .flatMapLatest { q ->
-            if (q.isBlank()) getTasksUseCase() else searchTasksUseCase(q)
+
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    private val _contentStateFlow: Flow<TaskContentState> = _searchQuery
+        .debounce(300)
+        .flatMapLatest { query ->
+            // Deciding which data flow will be used
+            val sourceFlow = if (query.isBlank()) getTasksUseCase() else searchTasksUseCase(query)
+
+            // 2. Local Flow ile "İlk Yükleme" durumunu birleştir
+            combine(sourceFlow, _isFirstLoad) { tasks, isFirstLoad ->
+
+                if (tasks.isNotEmpty()) {
+                    TaskContentState.Success(tasks)
+                } else {
+                    if (isFirstLoad) {
+                        TaskContentState.Loading
+                    } else {
+                        TaskContentState.Empty
+                    }
+                }
+            }
         }
         .flowOn(ioDispatcher)
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
-            emptyList()
-        )
+        // Error handling when get an error
+        .catch { emit(TaskContentState.Error(it.message ?: "Error")) }
 
-    fun setQuery(q: String) {
-        _query.value = q
+    val uiState: StateFlow<TaskListUiState> = combine(
+        _searchQuery,
+        _isSearchActive,
+        _contentStateFlow,
+        _isRefreshing
+    ) { query, isActive, content, refreshing ->
+        TaskListUiState(
+            searchQuery = query,
+            isSearchActive = isActive,
+            contentState = content,
+            isRefreshing = refreshing
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = TaskListUiState()
+    )
+
+
+    fun onEvent(event: TaskListUiEvent) {
+        when (event) {
+            is TaskListUiEvent.OnSearchQueryChange -> {
+                _searchQuery.value = event.query
+            }
+
+            is TaskListUiEvent.OnSearchActiveChange -> {
+                _isSearchActive.value = event.isActive
+                if (!event.isActive) _searchQuery.value = ""
+            }
+
+            is TaskListUiEvent.OnRefresh -> {
+                refreshData()
+            }
+        }
     }
 
-    fun refresh() {
-        viewModelScope.launch {
-            _isRefreshing.value = true
+    init {
+        refreshData(isInitial = true)
+    }
+
+    // Helper Function for data update
+    private fun refreshData(isInitial: Boolean = false) {
+        viewModelScope.launch(ioDispatcher) {
+            // Pull-2-refresh triggers its own spinner
+            if (!isInitial) _isRefreshing.value = true
+
             try {
                 refreshTasksUseCase()
             } catch (e: Exception) {
+                // One-time Toast message with Channel mechanism
+                val errorMessage =
+                    if (e is java.io.IOException) "No Internet Connection" else "${e.message}"
+                _effect.send(TaskListEffect.ShowToast(errorMessage))
                 e.printStackTrace()
             } finally {
+                delay(200)
                 _isRefreshing.value = false
+                _isFirstLoad.value = false
             }
         }
     }
